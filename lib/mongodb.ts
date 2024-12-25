9f29ef89744d0cc96e2c1ce7bffcca1ee3253d63
Adeleye Matthew <88755064+madegit@users.noreply.github.com>
2024-12-25 20:14:37 +0000

    eslint

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,18 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
+// Extend the global object type to include the mongoose property
 declare global {
   var mongoose: {
     conn: typeof mongoose | null;
     promise: Promise<typeof mongoose> | null;
-  } | undefined;
+  };
 }
 
+// Define the MongoDB connection URI
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
+// Use a cached global variable to reuse the connection
 let cached = global.mongoose;
 
 if (!cached) {
@@ -20,33 +25,28 @@ if (!cached) {
 }
 
 async function dbConnect() {
-  if (cached && cached.conn) { // Check if cached is defined and has a connection
+  if (cached.conn) {
+    // If a connection already exists, return it
     return cached.conn;
   }
 
-  if (cached && !cached.promise) {
+  if (!cached.promise) {
     const opts = {
       bufferCommands: false,
     };
+
+    // Create a new connection promise
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      // Assign the entire mongoose instance to cached.conn
-      cached.conn = mongoose; 
-      // Return the cached object, ensuring the correct type is returned.
-      return cached;
+      cached.conn = mongoose;
+      return mongoose;
     });
   }
 
   try {
-    if (cached) { // Check if cached is defined before accessing it
-      cached.conn = await cached.promise;
-    } else {
-      throw new Error('Cached variable is not defined.'); // Handle the case where cached is undefined
-    }
-  } catch (e) {
-    if (cached) { // Check if cached is defined before accessing it
-      cached.promise = null;
-    }
-    throw e;
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
   }
 
   return cached.conn;
